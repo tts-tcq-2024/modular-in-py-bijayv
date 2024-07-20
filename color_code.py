859a05dbe8bf24fa8492a486b5cc8b9bfcbf554b
bijayv <nrbijay52@gmail.com>
2024-07-20 20:40:23 +0530

    Update color_code.py

--- color_code.py
+++ color_code.py
@@ -1,9 +1,7 @@
 MAJOR_COLORS = ['White', 'Red', 'Black', 'Yellow', 'Violet']
 MINOR_COLORS = ["Blue", "Orange", "Green", "Brown", "Slate"]
-
 def color_pair_to_string(major_color, minor_color):
-  return f'{major_color} {minor_color}'
-  
+  return f'{major_color} {minor_color}'  
 def get_color_from_pair_number(pair_number):
   zero_based_pair_number = pair_number - 1
   major_index = zero_based_pair_number // len(MINOR_COLORS)
@@ -13,7 +11,6 @@ def get_color_from_pair_number(pair_number):
   if minor_index >= len(MINOR_COLORS):
     raise Exception('Minor index out of range')
   return MAJOR_COLORS[major_index], MINOR_COLORS[minor_index]
-  
 def get_pair_number_from_color(major_color, minor_color):
   try:
     major_index = MAJOR_COLORS.index(major_color)
@@ -24,13 +21,10 @@ def get_pair_number_from_color(major_color, minor_color):
   except ValueError:
     raise Exception('Minor index out of range')
   return major_index * len(MINOR_COLORS) + minor_index + 1
-  
-def test_number_to_pair(pair_number,
-                        expected_major_color, expected_minor_color):
+def test_number_to_pair(pair_number,expected_major_color, expected_minor_color):
   major_color, minor_color = get_color_from_pair_number(pair_number)
   assert(major_color == expected_major_color)
-  assert(minor_color == expected_minor_color)
-                          
+  assert(minor_color == expected_minor_color)                       
 def test_pair_to_number(major_color, minor_color, expected_pair_number):
   pair_number = get_pair_number_from_color(major_color, minor_color)
   assert(pair_number == expected_pair_number)
