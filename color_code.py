f86c02506696cd1836979a8c383bac3c4e698aa2
bijayv <nrbijay52@gmail.com>
2024-07-20 20:42:08 +0530

    Update color_code.py

--- color_code.py
+++ color_code.py
@@ -1,5 +1,3 @@
-MAJOR_COLORS = ['White', 'Red', 'Black', 'Yellow', 'Violet']
-MINOR_COLORS = ["Blue", "Orange", "Green", "Brown", "Slate"]
 def color_pair_to_string(major_color, minor_color):
   return f'{major_color} {minor_color}'  
 def get_color_from_pair_number(pair_number):
